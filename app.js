4ee0002b43f049c2ee3ae80dc50194529463ca54
matt-winzer <matt.winzer@gmail.com>
2018-08-14 13:42:47 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var numArray = [1, 3, 5, 7, 9];
+
+var objArray = [
+  { product: 'tennis ball', price: 3 },
+  { product: 'baseball', price: 5 },
+  { product: 'basketball', price: 10 },
+  { product: 'volleyball', price: 15 },
+  { product: 'football', price: 20 },
+];
+
+
+// Write a FOR loop that:
+  // iterates through numArray
+  // console.logs current value
+
+
+
+// Write a FOR loop that:
+  // console.logs current value multiplied by 3
+
+
+
+// Write a FOR loop that:
+  // iterates through objArray
+  // console.logs product name
+
+
+
+// Write a FOR loop that:
+  // iterates through objArray
+  // construct a sentence: console.log product name followed by its price
+  // SAMPLE: "football costs 20 dollars"
+
+
+
+// Write a FOR loop that:
+  // iterates through objArray
+  // only console.logs product names of products that have a price greater than 10
+
+
+
+// Write a FOR loop that:
+  // iterates BACKWARDS through numArray
+  // console.logs current value
+
