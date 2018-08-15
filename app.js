2c848941bf3c71b7979f30f94c295a4355f08591
mpcahn <mpcahn@gmail.com>
2018-08-15 10:35:50 -0600

    finish app.js

--- app.js
+++ app.js
@@ -13,34 +13,55 @@ var objArray = [
   // iterates through numArray
   // console.logs current value
 
-
+for (var i = 0; i < numArray.length; i++) {
+  console.log(numArray[i])
+}
 
 // Write a FOR loop that:
   // iterates through numArray
   // console.logs current value multiplied by 3
-
+  for (var i = 0; i < numArray.length; i++) {
+    console.log(numArray[i] * 3)
+  }
 
 
 // Write a FOR loop that:
   // iterates through objArray
   // console.logs product name
 
-
+  for (var i = 0; i < objArray.length; i++) {
+    console.log(objArray[i])
+  }
 
 // Write a FOR loop that:
   // iterates through objArray
   // construct a sentence: console.log product name followed by its price
   // SAMPLE: "football costs 20 dollars"
 
-
+for (var i = 0; i < objArray.length; i++) {
+    obj = objArray[i]
+    product = obj['product']
+    price = obj['price']
+    console.log(`${product} costs ${price} dollars`)
+  }
 
 // Write a FOR loop that:
   // iterates through objArray
   // only console.logs product names of products that have a price greater than 10
 
-
+for (var i = 0; i < objArray.length; i++) {
+  obj = objArray[i]
+  product = obj['product']
+  price = obj['price']
+  if (price > 10){
+    console.log(`${product} costs ${price} dollars`)
+  }
+}
 
 // Write a FOR loop that:
   // iterates BACKWARDS through numArray
   // console.logs current value
 
+for (var i = numArray.length - 1; i >= 0; i--){
+  console.log(numArray[i])
+}
